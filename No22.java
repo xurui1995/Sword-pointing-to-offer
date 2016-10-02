e46402a906141c38aa80ee7d24c38431c6538018
xurui1995 <ruixutcd@gmail.com>
2016-10-02 22:26:16 +0800

    10-2

--- No22.java
+++ No22.java
@@ -1,5 +1,7 @@
 import java.util.Stack;
 
+import javax.sound.sampled.Line;
+
 
 public class No22 {
 
@@ -12,17 +14,35 @@ public class No22 {
 	 */
 	public static void main(String[] args) {
 		Integer[] pushOrder={1,2,3,4,5};
-		Integer[] popOrder={4,5,3,2,1};
-		isRight(pushOrder,popOrder,5);
+		Integer[] popOrder={4,5,3,1,2};
+		System.out.println(isRight(pushOrder,popOrder,5));
 	}
 
 	private static boolean isRight(Integer[] pushOrder, Integer[] popOrder, int n) {
-			boolean isRight=false;
+			
 			Stack<Integer> stack=new Stack<Integer>();
+			int count=0;
 			for(int i=0;i<popOrder.length;i++){
-				
+				if(!stack.isEmpty()&&stack.peek()==popOrder[i])
+					stack.pop();
+				else{
+					if(count==pushOrder.length)
+						return false;
+					
+					else{
+						do{
+							stack.push(pushOrder[count++]);
+						}
+						while(stack.peek()!=popOrder[i]&&count!=pushOrder.length);
+						if(stack.peek()==popOrder[i])
+							stack.pop();
+						else{
+							return false;
+						}
+					}
+				}
 			}
-			return isRight;
+			return true;
 		
 	}
 
---
+++ No24.java
@@ -0,0 +1,14 @@
+
+public class No24 {
+
+	/**
+	 * 输入一个整数数组，
+	 * 判断该数组是不是某二叉搜索树的后序遍历的结果。
+	 * 如果是则返回true，否则返回false。
+	 * 假设输入的数组的任意两个数字都互不相同
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
